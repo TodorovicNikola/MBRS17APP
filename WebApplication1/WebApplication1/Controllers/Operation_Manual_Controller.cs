642b6a125449d6cb45d991cb554303f055307a9f
TodorovicNikola <nikola.todorovic@uns.ac.rs>
2017-06-18 23:39:04 +0200

    Manual code added

---
+++ WebApplication1/WebApplication1/Controllers/Operation_Manual_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public partial class OperationController : ApiController
+    {
+        [ResponseType(typeof(String))]
+        [HttpGet]
+        [Route("api/operations/getSomething")]
+        public IHttpActionResult GetSomething()
+        {
+            Faktura faktura = new Faktura();
+            return Ok(faktura.MethodThatReturnsSomething());
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Faktura_manual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public partial class Faktura
+    {
+        public String MethodThatReturnsSomething()
+        {
+            //Manually written method that should do something :)
+            return "Something";
+        }
+    }
+}
